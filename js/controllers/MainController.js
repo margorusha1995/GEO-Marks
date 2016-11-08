c6bec4407fdd603e07ce8122a27af40adea87055
margorusha1995 <margorusha1995@mail.ru>
2016-11-08 21:26:50 +0600

    M5

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,20 @@
 angular.module('Marks').controller("mainController", function ($scope) {
-    var myMap;
-    ymaps.ready(init);
-
-    function init () {
-        myMap = new ymaps.Map('map', {
-            center: [20.76, 37.64],
-            zoom: 10
-        }, 
-        {
-            searchControlProvider: 'yandex#search'
-        },
-        {
-            balloonMaxWidth: 200
-        });
-        $scope.map = myMap;
-    };
+//    var myMap;
+//    ymaps.ready(init);
+//
+//    function init () {
+//        myMap = new ymaps.Map('map', {
+//            center: [20.76, 37.64],
+//            zoom: 10
+//        }, 
+//        {
+//            searchControlProvider: 'yandex#search'
+//        },
+//        {
+//            balloonMaxWidth: 200
+//        });
+//        $scope.map = myMap;
+//    };
     
           
 });
